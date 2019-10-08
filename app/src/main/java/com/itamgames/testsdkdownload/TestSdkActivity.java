92c550fac072f621575ddcb067d9dab1126a38b1
itam-mac <famous@itam.games>
2019-10-08 09:42:52 +0900

    - new -

---
+++ app/src/main/java/com/itamgames/testsdkdownload/TestSdkActivity.java
@@ -0,0 +1,10 @@
+package com.itamgames.testsdkdownload;
+
+import android.util.Log;
+
+public class TestSdkActivity {
+
+    public void testHello() {
+        Log.e( "testsdkdownload" , "testHello");
+    }
+}
